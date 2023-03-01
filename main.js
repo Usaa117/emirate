c29231a4a65bf2e2713da7e62c4a482d811add60
Usaa117 <107350261+Usaa117@users.noreply.github.com>
2023-03-01 22:35:22 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+const featuredJobs = [
+    {
+      title: 'Front-end Developer',
+      description: 'We are seeking a skilled front-end developer to join our team.',
+      applyLink: 'https://www.example.com/apply'
+    },
+    {
+      title: 'Marketing Manager',
+      description: 'We are looking for an experienced marketing manager to lead our team.',
+      applyLink: 'https://www.example.com/apply'
+    },
+    {
+      title: 'Data Analyst',
+      description: 'We are hiring a data analyst to help us make data-driven decisions.',
+      applyLink: 'https://www.example.com/apply'
+    }
+  ];
+  
+  const allJobs = [
+    {
+      title: 'Back-end Developer',
+      description: 'We are looking for a back-end developer to build and maintain our servers.',
+      applyLink: 'https://www.example.com/apply'
+    },
+    {
+      title: 'UX Designer',
+      description: 'We are seeking a UX designer to create intuitive and user-friendly interfaces.',
+      applyLink: 'https://www.example.com/ 
+    
